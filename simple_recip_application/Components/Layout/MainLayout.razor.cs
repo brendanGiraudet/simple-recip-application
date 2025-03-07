d7b0bd5a8e134ccb18625da19f61dc739a07270d
baka <brendan.giraudet1@gmail.com>
2025-03-07 23:32:37 +0100

    set version and display application version

--- simple_recip_application/Components/Layout/MainLayout.razor.cs
+++ simple_recip_application/Components/Layout/MainLayout.razor.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Components;
+
 namespace simple_recip_application.Components.Layout;
 
 public partial class MainLayout
 {
-    
+    [Inject] public required IConfiguration Configuration { get; set; }
 }
