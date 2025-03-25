d8eee2b5dd63ab85c8195d1bd2984cbe04eef130
baka <brendan.giraudet1@gmail.com>
2025-03-25 11:11:59 +0100

    update card and recipeselector design, add page-grid to reuse grid css class

--- simple_recip_application/Features/RecipePlanningFeature/UserInterfaces/Components/RecipeSelector/RecipeSelector.razor.cs
+++ simple_recip_application/Features/RecipePlanningFeature/UserInterfaces/Components/RecipeSelector/RecipeSelector.razor.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Fluxor;
 using Microsoft.AspNetCore.Components;
 using simple_recip_application.Features.RecipesManagement.ApplicationCore.Entites;
@@ -13,10 +14,55 @@ public partial class RecipeSelector
 
     [Parameter] public EventCallback<IRecipeModel> OnRecipeSelected { get; set; }
 
+    private string _searchTerm = string.Empty;
+
     protected override void OnInitialized()
     {
         base.OnInitialized();
-        
-        Dispatcher.Dispatch(new LoadItemsAction<IRecipeModel>());
+
+        _searchTerm = string.Empty;
+
+        LoadFilteredRecipes();
+    }
+
+    private void OnSearchTermChanged(string searchTerm)
+    {
+        _searchTerm = searchTerm;
+        LoadFilteredRecipes();
+    }
+
+    private void LoadFilteredRecipes(int? skip = null)
+    {
+        Expression<Func<IRecipeModel, bool>>? filter = r => r.RemoveDate == null;
+
+        if (!string.IsNullOrEmpty(_searchTerm))
+            filter = i => i.Name.ToLower().Contains(_searchTerm.ToLower()) && i.RemoveDate == null;
+
+        Dispatcher.Dispatch(new LoadItemsAction<IRecipeModel>(Take: RecipeState.Value.Take, Skip: skip ?? 0, filter));
+    }
+
+    private string GetRecipesVisibilityCssClass() => !RecipeState.Value.IsLoading ? "" : "hidden";
+
+    private bool CanPreviousClick() => RecipeState.Value.Skip > 0;
+    private async Task OnPrevious()
+    {
+        if (!CanPreviousClick()) return;
+
+        var skip = RecipeState.Value.Skip - RecipeState.Value.Take;
+
+        skip = skip < 0 ? 0 : skip;
+
+        LoadFilteredRecipes(skip);
+
+        await Task.CompletedTask;
+    }
+
+    private async Task OnNext()
+    {
+        var skip = RecipeState.Value.Skip + RecipeState.Value.Take;
+
+        LoadFilteredRecipes(skip);
+
+        await Task.CompletedTask;
     }
 }
