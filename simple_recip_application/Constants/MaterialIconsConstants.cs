d9b7ee7e626e7d0ca4810ddd7fbb8849b6370670
baka <brendan.giraudet1@gmail.com>
2025-03-26 10:59:11 +0100

    add MaterialIconsConstants and use it

---
+++ simple_recip_application/Constants/MaterialIconsConstants.cs
@@ -0,0 +1,12 @@
+namespace simple_recip_application.Constants;
+
+public static class MaterialIconsConstants
+{
+    public const string List = "list";
+    public const string LeftArrow = "chevron_left";
+    public const string RightArrow = "chevron_right";
+    public const string Generate = "autorenew";
+    public const string Add = "add";
+    public const string CookingTime = "schedule";
+    public const string PreparationTime = "hourglass_top";
+}
